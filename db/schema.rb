492b5d6b6273ee93adcb1f3797230b85647ed29f
CodeKrakken <newholm1@hotmail.com>
2020-06-15 19:12:27 +0100

    Remove middlename column from the database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_20_121725) do
+ActiveRecord::Schema.define(version: 2020_01_15_130742) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2020_01_20_121725) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "company_id"
-    t.string "middlename"
     t.index ["company_id"], name: "index_employees_on_company_id"
   end
 
